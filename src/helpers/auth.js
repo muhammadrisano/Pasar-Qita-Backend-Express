0f0dcfddcbc79a91ec99f68dc5126d236ec9bcb0
Muhammad Risano Akbar <muhammadrisano@gmail.com>
2019-08-29 22:21:23 +0700

    hello

--- src/helpers/auth.js
+++ src/helpers/auth.js
@@ -4,19 +4,20 @@ const MiscHelper = require('../helpers/helpers')
 const allowedAccess = process.env.REQUEST_HEADERS
 
 module.exports = {
+
     authInfo: (req, res, next) => {
         const headerAuth = req.headers['authorization']
         const headerSecret = req.headers['x-access-token']
 
         if (headerAuth !== allowedAccess) {
-            return MiscHelper.response(res, null, 401, 'Unauthorized, Need Access Token !')
+            return MiscHelper.response(res, null, 401, 'Unauthorized, Need access token!')
         } else if (typeof headerSecret === 'undefined') {
             next()
         } else {
-            const bearerToken = headerSecret.splite(' ')
+            const bearerToken = headerSecret.split(' ')
             const token = bearerToken[1]
             req.token = token
-            console.log('Token stored ! ' + token)
+            console.log('Token stored !' + token)
             next()
         }
     },
@@ -27,11 +28,13 @@ module.exports = {
         console.log(accessToken);
         jwt.verify(accessToken, secretKey, (err, decoded) => {
             console.log(userToken);
+            // console.log(decoded.userid)
             if (err && err.name === 'TokenExpiredError') return MiscHelper.response(res, null, 401, 'Token Expired')
             if (err && err.name === 'JsonWebTokenError') return MiscHelper.response(res, null, 401, 'Invalid Token')
             if (parseInt(userToken) !== parseInt(decoded.userid)) return MiscHelper.response(res, null, 401, 'Invalid User Token')
             console.log(decoded)
             next()
+
         })
     }
 }
