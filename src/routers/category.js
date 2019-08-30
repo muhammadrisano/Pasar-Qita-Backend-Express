ac5ee1544c75951d9c3dc958a411ca353ceb1ce6
Muhammad Risano Akbar <muhammadrisano@gmail.com>
2019-08-30 10:59:39 +0700

    hello

--- src/routers/category.js
+++ src/routers/category.js
@@ -10,7 +10,7 @@ Route
     .get('/', Redis.cacheGetAllCategory, categoryController.getCategory)
     .get('/:id_category', Redis.clearGetAllCategory, categoryController.categoryDetail)
     .post('/', Redis.clearGetAllCategory, categoryController.insertCategory)
-    .patch('/:id_category', categoryController.updateCategory)
-    .delete('/:id_category', categoryController.deleteCategory)
+    .patch('/:id_category', Redis.clearGetAllCategory, categoryController.updateCategory)
+    .delete('/:id_category', Redis.clearGetAllCategory, categoryController.deleteCategory)
 
 module.exports = Route
